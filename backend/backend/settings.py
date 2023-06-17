136c5b943acdb342344e1fa968608f18f34c51f8
Степан Крылов <kiriki00@mail.ru>
2023-06-18 01:14:48 +0700

    some fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,6 +93,9 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
