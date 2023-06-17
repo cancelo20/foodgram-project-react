9e3a69d4556b7d7c131846bbd7a04539bd1e3bc7
Степан Крылов <kiriki00@mail.ru>
2023-06-17 11:43:25 +0700

    some fixes

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import IsAuthenticated
-# from rest_framework.pagination import PageNumberPagination
-# from rest_framework.serializers import ListSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.serializers import ListSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -25,6 +25,7 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     http_method_names = ('post', 'get', 'delete')
+    pagination_class = PageNumberPagination
 
     def get_permissions(self):
         if self.action in ['subscribe', 'unsubscribe', 'get_subscriptions']:
