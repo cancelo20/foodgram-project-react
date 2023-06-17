85197bdbd03db31cd3459bbe00a1d9eab8bb9ba5
Степан Крылов <kiriki00@mail.ru>
2023-06-17 23:44:52 +0700

    some fixes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 6,
     'SEARCH_PARAM': 'name'
 }
 
