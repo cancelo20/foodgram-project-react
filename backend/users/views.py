836b30538558284b1128eb03047db04dbd27104a
Степан Крылов <kiriki00@mail.ru>
2023-06-17 11:46:35 +0700

    some fixes

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,4 @@
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.serializers import ListSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
